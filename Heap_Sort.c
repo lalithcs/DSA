fdb9e0870c1726b01091f007ad54ab2504f5b281
Lalith <lalithchalla111@gmail.com>
2023-12-06 14:36:16 +0530

    New algorithsm and codes added

---
+++ Heap_Sort.c
@@ -0,0 +1,48 @@
+//heapsort
+#include <stdio.h>
+ void heapify(int arr[], int n, int i) {
+      int temp;
+      int largest = i;
+      int l = 2 * i + 1;
+      int r = 2 * i + 2;
+       if (l < n && arr[l] > arr[largest])
+         largest = l;
+       if (r < n && arr[r] > arr[largest])
+         largest = r;
+       if (largest != i) {
+         temp = arr[i];
+         arr[i] = arr[largest];
+         arr[largest] = temp;
+          heapify(arr, n, largest);
+      }
+   }
+ void heapSort(int arr[], int n) {
+   int temp;
+    for (int i = n / 2 - 1; i >= 0; i--)
+      heapify(arr, n, i);
+    for (int i = n - 1; i >= 0; i--) {
+      temp = arr[0];
+      arr[0] = arr[i];
+      arr[i] = temp;
+ 
+      heapify(arr, i, 0);
+   }
+}
+ 
+int main() {
+   int arr[10],n,i;
+     printf("enter no of elements");
+     scanf("%d",&n);
+     printf("enter elements");
+     for(i=0;i<n;i++)
+     scanf("%d",&arr[i]);
+    printf("Given array is: \n");
+   for (i = 0; i < n; i++)
+      printf("%d ", arr[i]);
+   printf("\n");
+    heapSort(arr, n);
+    printf("\nSorted array is: \n");
+   for (i = 0; i < n; ++i)
+      printf("%d ", arr[i]);
+}
+
--- Max_priority_queue.c
+++ Max_priority_queue.c
@@ -1,3 +1,4 @@
+//Max Priority Queue
 #include<stdio.h>
 #include<stddef.h>
 #include<stdlib.h>
